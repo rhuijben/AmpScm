70feb5a9b42b34307ddaef07444b88c878cfc8b3
Bert Huijben <bert@vmoo.com>
2022-01-28 17:16:55 +0100

    Walk trees towards subtrees and blobs

---
+++ src/Amp.Git.Client/GitClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amp.Git.Client
+{
+    public class GitClient : GitRepository
+    {
+        public GitClient(string path)
+            : base(InternalSetupArgs(path))
+        {
+        
+        }
+    }
+}
